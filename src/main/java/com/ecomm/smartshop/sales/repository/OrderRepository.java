5510500f8076c704463c9143da87dfaf3081bdf2
chehachraf <achrafchehboun@outlook.com>
2025-11-24 15:56:31 +0100

    add two methof 1 - find by client id 2 - exists by code id

--- src/main/java/com/ecomm/smartshop/sales/repository/OrderRepository.java
+++ src/main/java/com/ecomm/smartshop/sales/repository/OrderRepository.java
@@ -3,5 +3,10 @@ package com.ecomm.smartshop.sales.repository;
 import com.ecomm.smartshop.sales.entity.Commande;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderRepository extends JpaRepository<Commande,Long> {
+    List<Commande> findByClientId(Long clientId);
+
+    boolean existsByCodePromo(String codePromo);
 }
