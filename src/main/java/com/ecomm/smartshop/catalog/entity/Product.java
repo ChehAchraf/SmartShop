c2c14b324a1583dc46eba812e6d4c5d9011cb64c
chehachraf <achrafchehboun@outlook.com>
2025-11-24 12:25:52 +0100

    add product entity

--- src/main/java/com/ecomm/smartshop/catalog/entity/Product.java
+++ src/main/java/com/ecomm/smartshop/catalog/entity/Product.java
@@ -1,4 +1,37 @@
 package com.ecomm.smartshop.catalog.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@SQLDelete(sql = "UPDATE products SET is_deleted = true where id=?")
+@SQLRestriction("is_deleted = false")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nom;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @Column(nullable = false)
+    private Double prixUnitaire;
+
+    @Column(nullable = false)
+    private Integer stockDisponible;
+
+    @Column(nullable = false)
+    @Builder.Default
+    private Boolean isDeleted = false;
 }
