9e5ef996c6c5eddeb630bf2a4246c5bab33fb414
chehachraf <achrafchehboun@outlook.com>
2025-11-24 16:15:09 +0100

    create login fucntion that handle the buisnisse logic

--- src/main/java/com/ecomm/smartshop/identity/service/AuthService.java
+++ src/main/java/com/ecomm/smartshop/identity/service/AuthService.java
@@ -1,4 +1,24 @@
 package com.ecomm.smartshop.identity.service;
 
+import com.ecomm.smartshop.identity.dto.AuthRequest;
+import com.ecomm.smartshop.identity.dto.AuthResponse;
+import com.ecomm.smartshop.identity.entity.User;
+import com.ecomm.smartshop.identity.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
+    private final UserRepository userRepository;
+
+    public AuthResponse login(AuthRequest request){
+        User user = userRepository.findByUsername(request.username())
+                    .orElseThrow(()-> new RuntimeException("username or password is incorrect"));
+
+        if(!user.getPassword().equals(request.password())){
+            throw new RuntimeException("username or password is incorrect");
+        }
+        return new AuthResponse(user.getId(), user.getUsername(), user.getRole());
+    }
 }
