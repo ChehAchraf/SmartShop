e0eaa6f849914909c05554db9743611e7a63f9a8
chehachraf <achrafchehboun@outlook.com>
2025-11-25 14:02:48 +0100

    add client mapper

--- src/main/java/com/ecomm/smartshop/customer/mapper/ClientMapper.java
+++ src/main/java/com/ecomm/smartshop/customer/mapper/ClientMapper.java
@@ -1,8 +1,17 @@
 package com.ecomm.smartshop.customer.mapper;
 
+import com.ecomm.smartshop.customer.dto.ClientRequest;
+import com.ecomm.smartshop.customer.dto.ClientResponse;
+import com.ecomm.smartshop.identity.entity.Client;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface ClientMapper {
+
+    ClientResponse toResponse(Client client);
+
+    @Mapping(target = "id")
+    Client toEntity(ClientRequest request);
 
 }
