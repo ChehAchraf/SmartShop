4c3a3429ca4e76e0a38256054b6ba8a91604ed70
chehachraf <achrafchehboun@outlook.com>
2025-11-25 09:25:29 +0100

    create ClientResponse dto

---
+++ src/main/java/com/ecomm/smartshop/customer/dto/ClientResponse.java
@@ -0,0 +1,14 @@
+package com.ecomm.smartshop.customer.dto;
+
+import com.ecomm.smartshop.customer.enums.CustomerTier;
+
+public record ClientResponse(
+        Long id,
+        String nom,
+        String email,
+        String phone,
+        CustomerTier fidelityLevel,
+        Integer totalOrders,
+        Double totalSpent
+) {
+}
--- src/main/java/com/ecomm/smartshop/customer/mapper/ClientMapper.java
+++ src/main/java/com/ecomm/smartshop/customer/mapper/ClientMapper.java
@@ -1,8 +1,8 @@
-package com.ecomm.smartshop.customer.enums.mapper;
+package com.ecomm.smartshop.customer.mapper;
 
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface ClientMapper {
-    
+
 }
