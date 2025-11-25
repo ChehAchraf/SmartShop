7a9b10567da93bf3b4bc484d14024387c741c7dd
chehachraf <achrafchehboun@outlook.com>
2025-11-25 09:14:07 +0100

    add the webconfig for the infrastructure to handle intercepting the request

---
+++ src/main/java/com/ecomm/smartshop/infrastructure/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.ecomm.smartshop.infrastructure.config;
+
+import com.ecomm.smartshop.infrastructure.security.AuthInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@RequiredArgsConstructor
+public class WebConfig implements WebMvcConfigurer {
+    private final AuthInterceptor authInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns("/api/**");
+
+    }
+}
