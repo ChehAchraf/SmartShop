60e5ecf6021f316594c38bbf1dd51cec6eb0eee5
chehachraf <achrafchehboun@outlook.com>
2025-11-25 15:10:14 +0100

    add the function that verifier its an admin

---
+++ src/main/java/com/ecomm/smartshop/catalog/controller/ProductController.java
@@ -0,0 +1,38 @@
+package com.ecomm.smartshop.catalog.controller;
+
+import com.ecomm.smartshop.catalog.dto.ProductRequest;
+import com.ecomm.smartshop.catalog.dto.ProductResponse;
+import com.ecomm.smartshop.catalog.service.interfaces.ProductService;
+import com.ecomm.smartshop.shared.enums.UserRole;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+    private final ProductService service;
+
+    @PostMapping
+    public ResponseEntity<ProductResponse> create (@RequestBody @Valid ProductRequest request,
+                                                   HttpSession session){
+        verifierAdmin(session);
+    }
+
+
+
+    private void verifierAdmin(HttpSession session){
+        UserRole role = (UserRole) session.getAttribute("USER_ROLE");
+        if(role != UserRole.ADMIN){
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "access refused : you must be an admin ");
+        }
+    }
+}
