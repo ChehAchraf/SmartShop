60391db077952dc7d83f0c9e25f5ae48aaec4861
chehachraf <achrafchehboun@outlook.com>
2025-11-26 14:57:16 +0100

    Implement global exception handling with a custom `BusinessException`.

---
+++ src/main/java/com/ecomm/smartshop/shared/exception/customized/BusinessException.java
@@ -0,0 +1,7 @@
+package com.ecomm.smartshop.shared.exception.customized;
+
+public class BusinessException extends RuntimeException{
+    public BusinessException(String message){
+        super(message);
+    }
+}
