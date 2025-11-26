9d4279ca8c2e27475cbe986430a8a3a5dd0e0143
chehachraf <achrafchehboun@outlook.com>
2025-11-26 14:27:20 +0100

    add OrderService interface with methods for order creation, retrieval, and status updates

--- src/main/java/com/ecomm/smartshop/sales/service/interfaces/OrederService.java
+++ src/main/java/com/ecomm/smartshop/sales/service/interfaces/OrederService.java
@@ -1,5 +1,14 @@
 package com.ecomm.smartshop.sales.service.interfaces;
 
+import java.util.List;
+
+import com.ecomm.smartshop.sales.dto.CreateOrderRequest;
+import com.ecomm.smartshop.sales.dto.OrderResponse;
+
 public interface OrederService {
-    
+    OrderResponse createOrder(CreateOrderRequest request, Long clientId);
+    OrderResponse getOrderById(Long id);
+    List<OrderResponse> getMyOrders(Long clientId);
+    List<OrderResponse> getAllOrders(); 
+    void updateOrderStatus(Long orderId);
 }
