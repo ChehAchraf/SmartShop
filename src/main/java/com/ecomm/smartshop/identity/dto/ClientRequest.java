193be1d2c50d397d75076ac705c17a7d81d2b1ce
chehachraf <achrafchehboun@outlook.com>
2025-11-26 10:02:25 +0100

    add client request and response

--- src/main/java/com/ecomm/smartshop/identity/dto/ClientRequest.java
+++ src/main/java/com/ecomm/smartshop/identity/dto/ClientRequest.java
@@ -1,14 +1,20 @@
 package com.ecomm.smartshop.identity.dto;
 
 import com.ecomm.smartshop.customer.enums.CustomerTier;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 
 public record ClientRequest(
-        Long id,
+        @NotBlank(message = "Le nom est obligatoire")
         String nom,
+
+        @NotBlank(message = "L'email est obligatoire")
+        @Email(message = "Format d'email invalide")
         String email,
-        String phone,
-        CustomerTier fidelityLevel,
-        Integer totalOrders,
-        Double totalSpent
+
+        @NotBlank(message = "Le mot de passe est obligatoire")
+        String password,
+
+        String phone
 ) {
 }
