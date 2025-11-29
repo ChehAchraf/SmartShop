360258aa53ec6d68c89e15149030d5251030f1be
chehachraf <achrafchehboun@outlook.com>
2025-11-29 14:31:04 +0100

    add payment response

---
+++ src/main/java/com/ecomm/smartshop/billing/mapper/PaymentMapper.java
@@ -0,0 +1,19 @@
+package com.ecomm.smartshop.billing.mapper;
+
+import com.ecomm.smartshop.billing.dto.PaymentRequest;
+import com.ecomm.smartshop.billing.dto.PaymentResponse;
+import com.ecomm.smartshop.billing.entity.Paiement;
+import org.mapstruct.Mapping;
+
+public interface PaymentMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "numeroPaiement", ignore = true)
+    @Mapping(target = "datePaiement", ignore = true)
+    @Mapping(target = "dateEncaissement", ignore = true)
+    @Mapping(target = "statut", constant = "EN_ATTENTE")
+    @Mapping(target = "commande", ignore = true)
+    Paiement toEntity(PaymentRequest request);
+
+    @Mapping(target = "montantRestantCommande", source = "commande.montantRestant")
+    PaymentResponse toResponse(Paiement paiement);
+}
